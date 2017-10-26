e5a4a21f1559f5f6a65f9ba6894d3be845a8b985
Romario Mendoza <mendozaromario88@gmail.com>
2017-10-25 21:16:05 -0700

    game File
    2DGame

---
+++ Retribution/Assets/scripts/playerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+
+	public int playerSpeed=10;
+	public bool facingRight=true;
+	public int playerJumpPower=160;
+	private float moveX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		player_move();
+
+		
+	}
+	void player_move ()
+	{
+		moveX =Input.GetAxis("Horizontal");
+		if(Input.GetButtonDown("Jump"))
+		{
+			Jump ();
+		}
+		if( moveX < 0.0f && facingRight== false)
+		{
+			FlipPlayer();
+		}
+		else if(moveX > 0.0f && facingRight== true)
+		{
+			FlipPlayer();
+		
+		}
+
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D> ().velocity.y);
+	}
+	void Jump()
+	{
+		GetComponent<Rigidbody2D> ().AddForce (Vector2.up * playerJumpPower);
+	}
+	void FlipPlayer()
+	{
+		facingRight =! facingRight;
+		Vector2 localScale = gameObject.transform.localScale;
+		localScale.x *= -1;
+		transform.localScale = localScale;
+	}
+
+}
